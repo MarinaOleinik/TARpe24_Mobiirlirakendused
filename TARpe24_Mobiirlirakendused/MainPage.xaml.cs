6c7d6bb4e4f0a682df4fdf673bbc7fba73685ba2
marinaoleinik <marina.oleinik@tthk.ee>
2026-02-04 11:34:00 +0200

    1. Tund

--- TARpe24_Mobiirlirakendused/MainPage.xaml.cs
+++ TARpe24_Mobiirlirakendused/MainPage.xaml.cs
@@ -16,9 +16,41 @@
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
+                if(count==10){   
+                    BotImage.IsVisible = false;
+                    CounterLabel.Text = "Pilt kadus ära! Vajuta Reset.";}
                 CounterBtn.Text = $"Clicked {count} times";
 
+            BotImage.Rotation += 20;//Pöörab pilti iga vajutusega 20 kraadi
+
+            var rnd= new Random();            
+            var rndColor= Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            BackgroundColor = rndColor;//Muutab taustavärvi juhuslikuks värviks
+
+
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void ResetBtn_Clicked(object sender, EventArgs e)
+        {
+            count = 0;
+            CounterBtn.Text = "Alustame uuesti!";
+
+            BotImage.IsVisible = true;//Teeb pildi uuesti nähtavaks
+            BotImage.Rotation = 0;//Lähtestab pildi pöörde nulli
+            CounterLabel.Text="Pilt on tagasi!";
+
+            BackgroundColor = Colors.White;//muudame värv avalgeks
+            ResetBtn.ClearValue(BackgroundColorProperty);//eemaldame nupu taustavärvi
+            CounterBtn.ClearValue(Button.TextColorProperty);
+            if (BotImage.HorizontalOptions == LayoutOptions.Start)
+            {
+                BotImage.HorizontalOptions = LayoutOptions.End;
+            }
+            else
+            {
+                BotImage.HorizontalOptions = LayoutOptions.Start;
+            }
         }
     }
 }
